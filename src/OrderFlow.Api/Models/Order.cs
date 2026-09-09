8d68d1c7efe7770fd50462fa1f3c3920e6bd4bf0
InakiLE <inaki.lafuente.espeso@hotmail.com>
2026-09-09 12:17:49 +0200

    Add initial data model and EF Core setup

---
+++ src/OrderFlow.Api/Models/Order.cs
@@ -0,0 +1,10 @@
+namespace OrderFlow.Api.Models;
+
+public class Order
+{
+	public int Id { get; set; }
+	public DateTime CreatedAt { get; set; }
+	public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+    public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
--- src/OrderFlow.Api/Program.cs
+++ src/OrderFlow.Api/Program.cs
@@ -1,10 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using OrderFlow.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+builder.Services.AddControllers();
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
